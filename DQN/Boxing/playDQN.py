31bd76f45035a8c86006f0258284a3f69b570892
tomas <tomascanepa1397@gmail.com>
2022-08-31 16:32:23 -0300

    initial commit

---
+++ DQN/Boxing/playDQN.py
@@ -0,0 +1,31 @@
+import gym
+from stable_baselines3.dqn.policies import MlpPolicy, CnnPolicy, MultiInputPolicy
+from stable_baselines3 import DQN
+from stable_baselines3.common import atari_wrappers
+from stable_baselines3.common.env_util import make_atari_env
+from stable_baselines3.common.vec_env import VecFrameStack
+from stable_baselines3.common.evaluation import evaluate_policy
+import time
+import numpy as np
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+if __name__ == "__main__":
+    
+    juego = "BoxingNoFrameskip-v4"
+    env = make_atari_env(juego)
+    env = VecFrameStack(env, n_stack = 2)
+
+    root = tk.Tk()
+    root.withdraw()
+    nombremodelo = filedialog.askopenfilename(initialdir=os.getcwd())
+
+    modelo = DQN.load(nombremodelo)
+    modelo.set_env(env)
+
+    num_eval_episodes = 100
+
+    mean_reward, std_reward = evaluate_policy(modelo, modelo.get_env(), n_eval_episodes=num_eval_episodes, render = True)
+    print("\nEpisodios: " + str(num_eval_episodes) + "\nRecompensa media: " + str(mean_reward) + "\nDesviación estándar de recompensa: " + str(np.round(std_reward, 2)) + "\n")
+
---
+++ DQN/Boxing/trainDQN.py
@@ -0,0 +1,40 @@
+import gym
+from stable_baselines3.dqn.policies import MlpPolicy, CnnPolicy, MultiInputPolicy
+from stable_baselines3 import DQN
+from stable_baselines3.common import atari_wrappers
+from stable_baselines3.common.env_util import make_atari_env
+from stable_baselines3.common.vec_env import VecFrameStack
+from pathlib import Path
+import os
+
+if __name__ == "__main__":
+    
+    # Hiperparámetros
+
+    buffer_size = 100000 # tamaño de la memoria de replay
+    learning_rate = 0.00001 
+    batch_size = 32 # tamaño del subconjunto tomado del buffer de replay
+    learning_starts = 100000 # cuántos pasos en agente debería recolectar transiciones antes de empezar a aprender (coincide con la memoria de replay llena)
+    target_update_interval = 1000 # frecuencia en pasos en la que se actualiza la target network
+    train_freq = 4 # cantidad de pasos en las que el modelo se actualiza
+    gradient_steps = 1 
+    exploration_fraction = 0.1 # fracción en la que se va reduciendo la tasa de exploración
+    exploration_final_eps = 0.01 # valor final de la probabilidad de elegir una acción aleatoria
+    frames = 2 # tamaño del stack de frames
+
+    timesteps = 10000000
+    juego = "BoxingNoFrameskip-v4"
+
+    path = Path(os.getcwd())
+    tensorboard = str(path.parent) + "\dqn_atari_tensorboard"
+    env = make_atari_env(juego)
+
+    env = VecFrameStack(env, n_stack = frames)
+    nombremodelo = juego + "DQN"
+    
+    modelo = DQN(policy  = "CnnPolicy", env = env, verbose=1, tensorboard_log = tensorboard, device="cuda", buffer_size = buffer_size, learning_rate = learning_rate, batch_size = batch_size, learning_starts = learning_starts, target_update_interval = target_update_interval, train_freq = train_freq, gradient_steps = gradient_steps, exploration_fraction = exploration_fraction, exploration_final_eps = exploration_final_eps, optimize_memory_usage = True)
+    #modelo = DQN.load("./Modelos/" + nombremodelo)
+    modelo.set_env(env)
+    modelo.learn(total_timesteps=timesteps)
+    modelo.save("./Modelos/" + nombremodelo)
+    env.close()
